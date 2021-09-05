c21cdc77b856622e29b3299377f2527dd44b0685
philippeSimier <philaure@wanadoo.fr>
2021-09-05 11:04:33 +0200

    add methodes

--- programmes/testCarteSD/Msdcard.h
+++ programmes/testCarteSD/Msdcard.h
@@ -22,7 +22,14 @@ public:
     
     bool begin();
     void appendFile(fs::FS &fs, const char * path, const char * message);
-    void writeFile(fs::FS &fs, const char * path, const char * message);
+    void writeFile (fs::FS &fs, const char * path, const char * message);
+    void listDir   (fs::FS &fs, const char * dirname, uint8_t levels);
+    void createDir (fs::FS &fs, const char * path);
+    void removeDir (fs::FS &fs, const char * path);
+    void readFile  (fs::FS &fs, const char * path);
+    void renameFile(fs::FS &fs, const char * path1, const char * path2);
+    void deleteFile(fs::FS &fs, const char * path);
+    void testFileIO(fs::FS &fs, const char * path);
     
 private:
     
